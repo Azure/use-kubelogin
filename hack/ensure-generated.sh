07e202ce2e7dec2ec235f5ce6375b99ee101b896
hbc <bcxxxxxx@gmail.com>
2022-12-02 20:36:20 -0800

    fix: exit check on failure

--- hack/ensure-generated.sh
+++ hack/ensure-generated.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-npm run build
-git diff-index --quiet HEAD -- || echo "lib/index.js is not up to date. Please run 'npm run build' and commit the changes."
+npm run build 2>&1 >/dev/null
+git diff-index --quiet HEAD -- || echo "lib/index.js is not up to date. Please run 'npm run build' and commit the changes." && exit 1
