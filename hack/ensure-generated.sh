b130db8918f5aaa0974b1fd4a934b9392b1fdbfd
hbc <bcxxxxxx@gmail.com>
2022-12-02 20:50:08 -0800

    fix: check with git status

--- hack/ensure-generated.sh
+++ hack/ensure-generated.sh
@@ -3,5 +3,4 @@
 set -euo pipefail
 
 npm run build 2>&1 >/dev/null
-git update-index
-git diff-index --quiet HEAD -- || (echo "lib/index.js is not up to date. Please run 'npm run build' and commit the changes." && exit 1)
+git status --porcelain=v1 2>/dev/null | grep -q '^.M lib/index.js' && (echo "Generated files are out of date. Please run 'npm run build' and commit the changes." && exit 1) || exit 0
