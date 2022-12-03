f2b172e944d92308df0bd3c633a54b06a22e7a6d
hbc <bcxxxxxx@gmail.com>
2022-12-02 20:42:20 -0800

    fix: update git index first

--- hack/ensure-generated.sh
+++ hack/ensure-generated.sh
@@ -3,4 +3,5 @@
 set -euo pipefail
 
 npm run build 2>&1 >/dev/null
-git diff-index --quiet HEAD -- || echo "lib/index.js is not up to date. Please run 'npm run build' and commit the changes." && exit 1
+git update-index
+git diff-index --quiet HEAD -- || (echo "lib/index.js is not up to date. Please run 'npm run build' and commit the changes." && exit 1)
