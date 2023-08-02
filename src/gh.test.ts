f67732f8389836f7f38a347d9e2420a09207a162
hbc <bahe@microsoft.com>
2023-08-03 05:19:58 +0900

    fix: ignore GITHUB_API_URL (#4)
    octokit/action.js by default resolve the api base url via
    `GITHUB_API_URL`:
    
    
    https://github.com/octokit/action.js/blob/86bfb4edd3ac588b69fb46ee73a48c8bfd60ef7b/src/index.ts#L54-L57
    
    When running the action in GHEC environment, it will unable to discover
    the latest version from github.com .
    
    This pull request fixed by ignoring this environment variable by
    default. If an environment is required to use API endpoint other than
    `api.github.com`, user can override via input `github-api-base-url`.
    
    Resolve #3

--- src/gh.test.ts
+++ src/gh.test.ts
@@ -1,4 +1,9 @@
-import { isLatestVersion } from './gh';
+import { isLatestVersion, createOctoKitClient } from './gh';
+
+beforeAll(() => {
+  process.env['GITHUB_ACTION'] = 'github-action-test-env';
+  process.env['GITHUB_TOKEN'] = 'github-action-test-token';
+});
 
 test('isLatestVersion', () => {
   ['latest', 'LATEST', '', '*'].forEach((v) => {
@@ -8,4 +13,21 @@ test('isLatestVersion', () => {
   ['v0.0.24'].forEach((v) => {
     expect(isLatestVersion(v)).toBe(false);
   });
+});
+
+// issue #3
+test('createOctoKitClient - non-empty GITHUB_API_URL', () => {
+  // even if GITHUB_API_URL is set, it should be ignored
+  process.env['GITHUB_API_URL'] = 'https://foobar.com';
+  const client = createOctoKitClient();
+  expect(client.request.endpoint.DEFAULTS.baseUrl).toBe(
+    'https://api.github.com'
+  );
+});
+
+test('createOctoKitClient - with baseUrl override', () => {
+  const client = createOctoKitClient({
+    baseUrl: 'https://foobar.com',
+  });
+  expect(client.request.endpoint.DEFAULTS.baseUrl).toBe('https://foobar.com');
 });
